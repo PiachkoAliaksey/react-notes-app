c8982fa3a71fc39bc0a93f9f656015d14fc59792
PiachkoAliaksey <alek17y@mail.ru>
2023-03-21 20:06:09 +0300

    add: functional component NotesPage

---
+++ src/pages/NotesPage/NotesPage.tsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import NotesList from '../../components/NotesList/NotesList';
+
+
+
+
+const NotesPage:React.FC = () => {
+
+  return (
+    <>
+      <main className='notes-page'>
+        <h2 className='header-notes-page'>Notes Application</h2>
+        <NotesList/>
+      </main>
+    </>
+  )
+
+}
+
+export default NotesPage;
