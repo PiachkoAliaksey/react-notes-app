25ffa9d91b6f3f43ae4f868f72d1c99d4abb4282
PiachkoAliaksey <alek17y@mail.ru>
2023-03-21 20:00:45 +0300

    refactor: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes, redirect, Navigate, Link } from 'react-router-dom';
-import HomePage from 'pages/NotesPage';
+import NotesPage from './pages/NotesPage/NotesPage';
 
-import './sass/normalize.scss';
+
 import './sass/style.scss';
 
 
 function App() {
   return (
     <>
-      <HomePage/>
+      <NotesPage/>
     </>
   )
 }
